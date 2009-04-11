b785035de31f3da9288572e74dbb10b2a07a99ca
dougsko <dougtko@gmail.com>
2009-04-11 00:39:45 -0400

    spec/ruby-otp_spec.rb: got spec code coverage up to 100%

--- spec/ruby-otp_spec.rb
+++ spec/ruby-otp_spec.rb
@@ -11,15 +11,26 @@ describe "OTP" do
         fixture = @fixtures[algo]
         fixture.each do |expected_otp|
             otp = OTP.new(expected_otp["seq_num"], expected_otp["seed"], expected_otp["passphrase"], algo)
-            yield otp.to_s, expected_otp["sentence"]
+            yield otp, expected_otp["sentence"]
         end
     end
 
     ["md4", "md5", "sha1"].each do |algo|
         it "Tests #{algo}" do
-            testit(algo) do |result, expected|
-                result.should == expected
+            testit(algo) do |otp, expected|
+                otp.to_s.should == expected
             end
+        end
+        it "Tests hex output" do
+            testit(algo) do |otp, expected|
+                otp.to_hex.should =~ /[0-9A-F]{16}$/ 
+            end
+        end
+    end
+
+    it "Tests seed generator" do
+        10.times do
+            OTP.generate_seed.should =~ /^[a-z]{2}\d{4}$/
         end
     end
 end
